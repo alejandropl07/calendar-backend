135c03a3b2066de25a49a395c91c9c837d589247
alejandropl07 <aprietoleon97@gmail.com>
2022-07-19 14:09:12 -0500

    Config express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Backend");
+const express = require("express");
+
+//Express server
+const app = express();
+
+//Routes
+app.get("/", (req, res) => {
+  res.json({
+    ok: true,
+  });
+});
+
+//Listen requests
+app.listen(4000, () => {
+  console.log(`Server running on port ${4000}`);
+});
