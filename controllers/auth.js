6151787674bf453ad62c6d708b94a1b21b46ce5a
alejandropl07 <aprietoleon97@gmail.com>
2022-07-20 01:17:20 -0500

    Express validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,48 @@
 const { response } = require("express");
+const { validationResult } = require("express-validator");
 
 const createUser = (req, res = response) => {
-  res.json({
+  const { name, email, password } = req.body;
+
+  //Handle errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
+
+  res.status(201).json({
     ok: true,
     msg: "Register",
+    name,
+    email,
+    password,
   });
 };
 
-const login = (req, res) => {
+const login = (req, res = response) => {
+  //Handle errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
+
+  const { email, password } = req.body;
+
   res.json({
     ok: true,
     msg: "Login",
+    email,
+    password,
   });
 };
 
-const revalidateJwt = (req, res) => {
+const revalidateJwt = (req, res = response) => {
   res.json({
     ok: true,
     msg: "Renew",
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,30 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
 const { createUser, login, revalidateJwt } = require("../controllers/auth");
 const router = Router();
 
-router.post("/new", createUser);
+router.post(
+  "/new",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password must have at least 6 characters").isLength({
+      min: 6,
+    }),
+  ],
+  createUser
+);
 
-router.post("/", login);
+router.post(
+  "/",
+  [
+    check("email", "Email is required").isEmail(),
+    check("password", "Password must have at least 6 characters").isLength({
+      min: 6,
+    }),
+  ],
+  login
+);
 
 router.get("/renew", revalidateJwt);
 
