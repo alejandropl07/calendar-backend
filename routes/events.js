45b324b8986bf9602f32ddadb532cdf9da7ea4fe
alejandropl07 <aprietoleon97@gmail.com>
2022-07-21 11:54:37 -0500

    Events validated

--- routes/events.js
+++ routes/events.js
@@ -1,17 +1,29 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
 const {
   getEvents,
   createEvent,
   updateEvent,
   deleteEvent,
 } = require("../controllers/events");
+const { isDate } = require("../helpers/isDate");
+const { validateFields } = require("../middlewares/fields-validator");
 const { validateJWT } = require("../middlewares/jwt-validator");
 const router = Router();
 
 router.use(validateJWT);
 
 router.get("/", getEvents);
-router.post("/", createEvent);
+router.post(
+  "/",
+  [
+    check("title", "Title is required").not().isEmpty(),
+    check("start", "Start date is required").custom(isDate),
+    check("end", "End date is required").custom(isDate),
+    validateFields,
+  ],
+  createEvent
+);
 router.put("/:id", updateEvent);
 router.delete("/:id", deleteEvent);
 
