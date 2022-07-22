cf5c9d3827350ad7edddf323c3377b612a349ac0
alejandropl07 <aprietoleon97@gmail.com>
2022-07-21 20:34:29 -0500

    createEvent done

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const Event = require("../models/Event");
 
 const getEvents = async (req, res = response) => {
   res.json({
@@ -8,11 +9,22 @@ const getEvents = async (req, res = response) => {
 };
 
 const createEvent = async (req, res = response) => {
-  console.log(req.body);
-  res.json({
-    ok: true,
-    msg: "createEvent",
-  });
+  const event = new Event(req.body);
+
+  try {
+    event.user = req.uid;
+    const eventDB = await event.save();
+    res.json({
+      ok: true,
+      event: eventDB,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: true,
+      msg: "Error",
+    });
+  }
 };
 
 const updateEvent = async (req, res = response) => {
